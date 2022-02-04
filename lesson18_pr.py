e1071e174941c276d57c65a3dcc09a951c39e696
volodymyr <vladimir.zontov@gmail.com>
2022-02-04 21:21:28 +0200

    lesson19

--- lesson18_pr.py
+++ lesson18_pr.py
@@ -1,14 +1,36 @@
 import requests
 from requests_oauthlib import OAuth1
 import os
+from urllib.parse import urljoin
 from dotenv import load_dotenv
 
 load_dotenv()
 
-auth = OAuth1(os.getenv('KEY'), os.getenv('SECRET'))
-endpoint = "http://api.thenounproject.com/icon/1"
 
-response = requests.get(endpoint, auth=auth)
-print(response.content)
+class IconGetter:
+    def __init__(self):
+        self._auth = OAuth1(os.getenv('KEY'), os.getenv('SECRET'))
+        self._endpoint = "http://api.thenounproject.com/icon/"
+        self.icon = None
+
+    def get_icon(self, id):
+        url = urljoin(self._endpoint, str(id))
+        response = requests.get(url, auth=self._auth)
+        result = response.json()
+        icon_url = result['icon']['icon_url']
+        # self.icon = requests.get(icon_url).content
+        self.icon = self._get_icon(icon_url)
+
+    def _get_icon(self, icon_url):
+        response = requests.get(icon_url)
+        return response.content
+
+    def write_icon(self, filename):
+        with open(filename, 'wb') as file:
+            file.write(self.icon)
 
 
+icon_getter = IconGetter()
+icon_getter.get_icon("1")
+print(icon_getter.icon)
+icon_getter.write_icon("alphabet/test.svg")
